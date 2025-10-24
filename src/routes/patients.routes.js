9222466255a5716e9e1a78159612a84e043f25ed
GRAIX12 <90628338+GRAIX12@users.noreply.github.com>
2025-10-23 21:21:40 -0700

    =commit

--- src/routes/patients.routes.js
+++ src/routes/patients.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const patientController = require('../controllers/patientController');
+const patientController = require('../controllers/patients.controller');
 
 module.exports = router;
 
